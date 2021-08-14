b0ba6d02d173add4e52149a7ecc6441e0539f0a3
JHSeo <qnfqnfqnf@gmail.com>
2021-08-14 23:28:22 +0900

    [Add] figma api continue...

--- packages/seodestem-figma-api/src/endpoints/index.ts
+++ packages/seodestem-figma-api/src/endpoints/index.ts
@@ -1,1 +1,83 @@
-export const endpoinsts = {};
+import { Paths } from './path-routes';
+import {
+  ExtractRequest,
+  ExtractResponses,
+  MethodsMap,
+  RequestHeaders,
+} from './types';
+
+export type Operation<
+  Url extends keyof Paths,
+  Method extends keyof Paths[Url]
+> = {
+  parameters: ExtractRequest<Paths[Url][Method]>;
+  request: {
+    method: Method extends keyof MethodsMap ? MethodsMap[Method] : never;
+    url: Url;
+    headers: RequestHeaders;
+  };
+  responses: ExtractResponses<Paths[Url][Method]>;
+};
+
+export const EndpointRoutes = [
+  /**
+   * files
+   */
+  'GET /v1/files/:key',
+  'GET /v1/files/:key/nodes',
+  'GET /v1/images/:key',
+  'GET /v1/files/:key/images',
+
+  /**
+   * comments
+   */
+  'GET /v1/files/:key/comments',
+  'POST /v1/files/:file_key/comments',
+  'DELETE /v1/files/:file_key/comments',
+
+  /**
+   * users
+   */
+  'GET /v1/me',
+
+  /**
+   * version history
+   */
+  'GET /v1/files/:key/versions',
+
+  /**
+   * projects
+   */
+  'GET /v1/teams/:team_id/projects',
+  'GET /v1/projects/:project_id/files',
+
+  /**
+   * comnponents and styles
+   */
+  'GET /v1/teams/:team_id/components',
+  'GET /v1/files/:file_key/components',
+  'GET /v1/components/:key',
+  'GET /v1/teams/:team_id/component_sets',
+  'GET /v1/files/:file_key/component_sets',
+  'GET /v1/component_sets/:key',
+  'GET /v1/teams/:team_id/styles',
+  'GET /v1/files/:file_key/styles',
+  'GET /v1/styles/:key',
+];
+
+export type Endpoints = {
+  /**
+   * files
+   */
+  'GET /v1/files/:key': Operation<'/v1/files/:key', 'get'>;
+  'GET /v1/files/:key/nodes': Operation<'/v1/files/:key/nodes', 'get'>;
+  'GET /v1/images/:key': Operation<'/v1/files/:key/nodes', 'get'>;
+  'GET /v1/files/:key/images': Operation<'/v1/files/:key/nodes', 'get'>;
+
+  /**
+   * comments
+   */
+  'GET /v1/files/:key/comments': Operation<'/v1/files/:key', 'get'>;
+  'POST /v1/files/:file_key/comments': Operation<'/v1/files/:key', 'get'>;
+  'DELETE /v1/files/:file_key/comments': Operation<'/v1/files/:key', 'get'>;
+};
--- packages/seodestem-figma-api/src/endpoints/types.ts
+++ packages/seodestem-figma-api/src/endpoints/types.ts
@@ -1,61 +1,92 @@
-export type Paths = {
-  '/v1/files/:key': {
-    get: {
-      parameters: {
-        path: {
-          key: string;
-          version?: string;
-          ids?: string;
-          depth?: number;
-          geometry?: string;
-          plugin_data?: string;
-          branch_data?: string;
-        };
-      };
-      responses: {
-        200: {
-          content: {
-            name: String;
-            role: String;
-            lastModified: String;
-            thumbnailUrl: String;
-            version: String;
-            document: NodeType;
-            components: Map<String, ComponentNode>;
-            schemaVersion: 0;
-            styles: Map<String, StyleType>;
-            mainFileKey: String;
-            branches: [
-              {
-                key: String;
-                name: String;
-                thumbnail_url: String;
-                last_modified: String;
-              }
-            ];
-          };
-        };
-        404: {
-          content: {
-            message?: string;
-            url?: string;
-            status?: string;
-          };
-        };
-      };
-    };
-  };
-  '/v1/files/:key/:title': {
-    get: {
-      parameters: {
-        path: {
-          key: string;
-          title: string;
-        };
-      };
-      responses: {
-        200: {};
-      };
-    };
-  };
+// https://stackoverflow.com/a/50375286/206879
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
+type ExtractRequestParameters<T> = 'parameters' extends keyof T
+  ? UnionToIntersection<
+      {
+        [K in keyof T['parameters']]: T['parameters'][K];
+      }[keyof T['parameters']]
+    >
+  : {};
+
+type ExtractRequestBody<T> = 'requestBody' extends keyof T
+  ? UnionToIntersection<
+      {
+        [K in keyof T['requestBody']]: T['requestBody'][K];
+      }[keyof T['requestBody']]
+    >
+  : {};
+
+export type RequestHeaders = {
+  'X-FIGMA-TOKEN'?: string;
+  [header: string]: string | number | undefined;
+};
+
+type ResponseHeaders = {
+  'cache-control'?: string;
+  'content-length'?: number;
+  'content-type'?: string;
+  date?: string;
+  etag?: string;
+  'last-modified'?: string;
+  link?: string;
+  location?: string;
+  server?: string;
+  status?: string;
+  vary?: string;
+
+  [header: string]: string | number | undefined;
+};
+
+type APIResponse<T, S extends number = number> = {
+  headers: ResponseHeaders;
+  status: S;
+  url: string;
+  data: T;
+};
+
+type SuccessStatuses = 200 | 201 | 202 | 204;
+
+type ExtractContentKey<T> = 'content' extends keyof T
+  ? {
+      [K in keyof T['content']]: T['content'][K];
+    }[keyof T['content']]
+  : T;
+
+type SuccessResponseDataType<Responses> = {
+  [K in SuccessStatuses & keyof Responses]: ExtractContentKey<
+    Responses[K]
+  > extends never
+    ? never
+    : APIResponse<ExtractContentKey<Responses[K]>, K>;
+};
+
+/**
+ * type Extract Request
+ */
+export type ExtractRequest<T> = ExtractRequestParameters<T> &
+  ExtractRequestBody<T>;
+
+/**
+ * type Extract Response
+ */
+export type ExtractResponses<R> = 'responses' extends keyof R
+  ? SuccessResponseDataType<R['responses']> extends never
+    ? never
+    : SuccessResponseDataType<R['responses']>
+  : unknown;
+
+/**
+ * type Methods
+ */
+export type MethodsMap = {
+  delete: 'DELETE';
+  get: 'GET';
+  patch: 'PATCH';
+  post: 'POST';
+  put: 'PUT';
 };
